9bb9241fa3ab95b0da19d5febee54970bd5e4af9
imgurbot12 <imgurbot12@gmail.com>
2023-07-24 10:03:19 -0700

    chore: include requirements and add dependency

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ setup(
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
     description='Unify SocketServer Implementations based on a Session Model',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     long_description=readme,
     long_description_content_type="text/markdown",
+    install_requires=['pyderive3>=0.0.6'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
